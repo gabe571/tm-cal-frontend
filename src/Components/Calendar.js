30ad9f0e106d63991e203a8f831c05cc399a3a09
Gabe571 <gabriel.castro1028@gmail.com>
2020-12-24 12:27:51 -0800

    useState, useEffect

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import moment from "moment"
 import CalStyles from "../CalStyles.css"
 
 export default function Calendar() {
-    const value = moment() //Javascript Library
+    const [calendar, setCalendar] = useState([]) //matrix of calendar weeks and days
+    const [value, setValue] = useState(moment()); //current day selected
+
     const firstDay = value.clone().startOf('month').startOf('week') //Start date of week
     const lastDay = value.clone().endOf('month').endOf('week') //Last day of the week
-    const day = firstDay.clone().subtract(1, "day" );
-    const calendar = []
 
-    while(day.isBefore(lastDay, "day")) {  //whileloop to iterate over days
-        calendar.push(
-            Array(7).fill(0).map(() => day.add(1, "day").clone()) //matrix (array of arrays)
-        )
-    } // mapping over each day of the month
+    useEffect(() => {
+        const day = firstDay.clone().subtract(1, "day" );
+        const d = [] //empty array useed to store day
+        while(day.isBefore(lastDay, "day")) {  //whileloop to iterate over days
+            d.push(
+                Array(7)
+                .fill(0)
+                .map(() => day.add(1, "day").clone()) //matrix (array of arrays)
+            )
+        } // mapping over each day of the month
+        setCalendar(d)
+    }, [value]) //calendar day being re-rendered each day
+
 return ( 
  <div className="calendar">  
   {calendar.map(week => 
